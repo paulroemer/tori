080fc2daaedd38e917fb7b4ffbd6719f1450ffaf
„Wolfgang <wolfgang@Wolfgangs-MacBook-Pro.fritz.box>
2016-05-26 18:29:11 +0200

    made tory display threads with 'vaadincom' profile (still one stacktrace and no transactions for write)

--- webapp/src/main/java/com/liferay/portal/patch/UserPersistenceImplPatch.java
+++ webapp/src/main/java/com/liferay/portal/patch/UserPersistenceImplPatch.java
@@ -1,6 +1,7 @@
 package com.liferay.portal.patch;
 
-import com.liferay.portal.service.persistence.UserGroupPersistenceImpl;
+import com.liferay.portal.service.persistence.GroupPersistence;
+import com.liferay.portal.service.persistence.RolePersistence;
 import com.liferay.portal.service.persistence.UserPersistenceImpl;
 import org.springframework.beans.factory.InitializingBean;
 
@@ -8,4 +9,11 @@ import org.springframework.beans.factory.InitializingBean;
  * Created by wolfgang on 23/05/16.
  */
 public class UserPersistenceImplPatch extends UserPersistenceImpl implements InitializingBean {
+	public void setGroupPersistence(GroupPersistence groupPersistence) {
+		this.groupPersistence = groupPersistence;
+	}
+	public void setRolePersistence(RolePersistence rolePersistence) {
+		this.rolePersistence = rolePersistence;
+	}
+
 }
