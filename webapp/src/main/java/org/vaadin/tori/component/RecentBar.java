f45befb97d634cff8033e30ac171a733eacf060f
Paul Römer <paul@vaadin.com>
2017-04-05 15:19:22 +0200

    most recent post link fixed

--- webapp/src/main/java/org/vaadin/tori/component/RecentBar.java
+++ webapp/src/main/java/org/vaadin/tori/component/RecentBar.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2014 Vaadin Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -199,7 +199,7 @@ public class RecentBar extends CustomComponent implements UserAuthoredListener {
         private static String getPermaLinkUrl(final Post post) {
             // @formatter:off
             final String linkUrl = String.format(
-                    "#%s/%s/%s",
+                    "forum/#%s/%s/%s",
                     ToriNavigator.ApplicationView.THREADS.getUrl(),
                     post.getThread().getId(),
                     post.getId()
